c4fb5ffb6c05181af5602c2202c63002adc4d47d
Yash Bele <krupabele47@gmail.com>
2026-03-12 17:37:18 +0530

    fixed mail

--- server/config/email.js
+++ server/config/email.js
@@ -4,10 +4,16 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const transporter = nodemailer.createTransport({
-  service: "gmail", // or your SMTP provider
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  requireTLS: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
+  },
+  tls: {
+    rejectUnauthorized: false,
   },
 });
 
